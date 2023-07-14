042c1f1a335711d6931fff19650c24f112056c78
Xabi Etxeberria <xabier.etxeberria93@gmail.com>
2023-07-14 22:10:11 +0200

    Customize spaceship prompt

--- shell/zsh/themes/spaceship.sh
+++ shell/zsh/themes/spaceship.sh
@@ -1,3 +1,9 @@
 # ------------------------------------------------------------------------------
 # Spaceship prompt config
 # ------------------------------------------------------------------------------
+export SPACESHIP_DIR_TRUNC_REPO=false       # Don't truncate the directory path in Git repositories
+export SPACESHIP_GIT_BRANCH_COLOR=208       # Change Git Branch section color to orange (keep red for status)
+export SPACESHIP_PACKAGE_SHOW=false         # Hide Package section
+export SPACESHIP_NODE_PREFIX="| "           # Switch "via" prefix for | in the Node section
+export SPACESHIP_DOCKER_PREFIX="| "         # Switch "on" prefix for | in the Docker section
+export SPACESHIP_DOCKER_COMPOSE_PREFIX="| " # Switch "runs" prefix for | in the Docker Compose section
