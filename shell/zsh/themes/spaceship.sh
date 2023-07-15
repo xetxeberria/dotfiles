4c103588c6bceaa232215ee2f3e1f43e59012af4
Xabi Etxeberria <xabier.etxeberria93@gmail.com>
2023-07-15 08:54:58 +0200

    Remove spaceship unused sections

--- shell/zsh/themes/spaceship.sh
+++ shell/zsh/themes/spaceship.sh
@@ -3,7 +3,49 @@
 # ------------------------------------------------------------------------------
 export SPACESHIP_DIR_TRUNC_REPO=false       # Don't truncate the directory path in Git repositories
 export SPACESHIP_GIT_BRANCH_COLOR=208       # Change Git Branch section color to orange (keep red for status)
-export SPACESHIP_PACKAGE_SHOW=false         # Hide Package section
 export SPACESHIP_NODE_PREFIX="| "           # Switch "via" prefix for | in the Node section
 export SPACESHIP_DOCKER_PREFIX="| "         # Switch "on" prefix for | in the Docker section
 export SPACESHIP_DOCKER_COMPOSE_PREFIX="| " # Switch "runs" prefix for | in the Docker Compose section
+
+# Remove unused sections
+spaceship remove time
+spaceship remove user
+spaceship remove host
+spaceship remove hg
+spaceship remove package
+spaceship remove bun
+spaceship remove deno
+spaceship remove ruby
+spaceship remove python
+spaceship remove elm
+spaceship remove elixir
+spaceship remove xcode
+spaceship remove swift
+spaceship remove golang
+spaceship remove perl
+spaceship remove rust
+spaceship remove haskell
+spaceship remove scala
+spaceship remove java
+spaceship remove lua
+spaceship remove dart
+spaceship remove julia
+spaceship remove crystal
+spaceship remove aws
+spaceship remove gcloud
+spaceship remove azure
+spaceship remove venv
+spaceship remove conda
+spaceship remove dotnet
+spaceship remove ocaml
+spaceship remove vlang
+spaceship remove zig
+spaceship remove purescript
+spaceship remove erlang
+spaceship remove ansible
+spaceship remove terraform
+spaceship remove pulumi
+spaceship remove ibmcloud
+spaceship remove nix_shell
+spaceship remove gnu_screen
+spaceship remove battery
